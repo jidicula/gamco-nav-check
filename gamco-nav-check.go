c0718cbf172bbc897fc43cd743150d76d45f109d
jidicula <johanan@forcepush.tech>
2021-04-10 14:29:09 -0400

    feat: Add function for extracting NAVs
    Resolves: #1

--- gamco-nav-check.go
+++ gamco-nav-check.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jidicula/go-gamco"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+}
+
+// getNAVs returns a map of NAVs for each GAMCO common stock.
+func extractNAVs(fl []gamco.Fund) map[string]string {
+	navs := make(map[string]string)
+	for _, v := range fl {
+		navs[v.Symbol] = v.NAV
+	}
+	return navs
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gamco-nav-check
 
 go 1.16
+
+require github.com/jidicula/go-gamco v0.3.0
